d70a61b67ed151128a269985b6747a5a380a7910
TshiamoB <tshiamob@miway.co.za>
2021-03-13 18:42:16 +0200

    Only allow sending infomation via POST - Block user sending information via any other http method beside post - Use the event (info sent to api) as the payload to rabbitmq - always close the channel & amqp connections - added logger messages

--- functions/save-reciept.js
+++ functions/save-reciept.js
@@ -2,16 +2,31 @@ const amqp = require('amqplib');
 
 const url = "amqps://xrgawglz:9bwYNG_BuoeVu10TBSY70fW6trZdHVVp@rattlesnake.rmq.cloudamqp.com/xrgawglz";
 
-exports.handler = async () => {
+exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+        console.error('[x] The HTTP method is not a POST');
+
+        return {
+            statusCode: 405,
+            body: 'Method not allowed',
+        };
+    }
 
     try {
         const conn = await amqp.connect(url);
         const channel = await conn.createChannel();
 
-        const queue = 'hello';
-        const msg = 'Hello World';
-        channel.assertQueue(queue, { durable: false });
-        channel.sendToQueue(queue, Buffer.from(msg));
+        //TODO: Add logic to check if event.body has content
+
+        const queue = 'raw-data';
+        const msg = event.body;
+        await channel.assertQueue(queue, { durable: true });
+        channel.sendToQueue(queue, Buffer.from(msg), { persistent: true });
+
+        console.log('[o] The following message was successfully sent to the %s queue:', queue, msg);
+
+        await channel.close();
+        await conn.close();
 
         return {
             statusCode: 200,
