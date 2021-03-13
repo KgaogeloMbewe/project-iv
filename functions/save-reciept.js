047f1b09baec31ef3c4d7ba224baf519e5333992
TshiamoB <tshiamob@miway.co.za>
2021-03-13 17:54:05 +0200

    Added mock queue setup to test connection - Use amqplib to connnect to our rabbitmq instance - send a message to a dummy queue & respons with 200 when successful - response with a 500 when an error is encountered

--- functions/save-reciept.js
+++ functions/save-reciept.js
@@ -1,8 +1,31 @@
-exports.handler = async function () {
+const amqp = require('amqplib');
 
+const url = "amqps://xrgawglz:9bwYNG_BuoeVu10TBSY70fW6trZdHVVp@rattlesnake.rmq.cloudamqp.com/xrgawglz";
 
-    return {
-        statusCode: 200,
-        body: "Hello World",
-    };
+exports.handler = async () => {
+
+    try {
+        const conn = await amqp.connect(url);
+        const channel = await conn.createChannel();
+
+        const queue = 'hello';
+        const msg = 'Hello World';
+        channel.assertQueue(queue, { durable: false });
+        channel.sendToQueue(queue, Buffer.from(msg));
+
+        return {
+            statusCode: 200,
+            body: "Hello World",
+        };
+    } catch (error) {
+        console.log(error);
+
+        return {
+            statusCode: 500,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify('An Error occurred'),
+        }
+    }
 };
