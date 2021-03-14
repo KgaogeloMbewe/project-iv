88f03e12e63d8e9e0f8df72be81d5f4150e21128
Tshiamo <tshiamobhuda@gmail.com>
2021-03-14 16:30:49 +0200

    Added logic to connect to queue & consume a message

--- functions/process-receipt.js
+++ functions/process-receipt.js
@@ -7,7 +7,22 @@ const url = process.env.RABBITMQ_URL;
 
 exports.handler = async () => {
     try {
-        //TODO add logic to pull items from queue & process them
+        const conn = await amqp.connect(url);
+        const channel = await conn.createChannel();
+        const queue = 'raw-data';
+
+        await channel.assertQueue(queue, {durable: true});
+        // await channel.prefetch(1); TODO uncomment before processing data
+
+        console.log('[<--__-->] Waiting for messages in the %s. queue', queue);
+
+        const msg = await channel.consume(queue, null, {noAck: false});
+
+        //TODO Add logic to process the raw receipt
+        //TODO send the processed receipt to the processed queue
+
+        await channel.close();
+        await conn.close();
 
         return {
             statusCode: 200,
