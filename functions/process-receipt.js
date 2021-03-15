a4216b988fb3033747e1dca000555dfadbf280e0
Tshiamo <tshiamobhuda@gmail.com>
2021-03-16 01:25:09 +0200

    Added Logic to process raw receipts -  Added functions to mine data from a receipt, data mined includes products, store info and payment info - build a processed receipt from mined data - remove unprocessed receipet from queue once processed - send processed receipt to processed-data queue

--- functions/process-receipt.js
+++ functions/process-receipt.js
@@ -12,25 +12,47 @@ exports.handler = async () => {
         const queue = 'raw-data';
 
         await channel.assertQueue(queue, {durable: true});
-        // await channel.prefetch(1); TODO uncomment before processing data
+        await channel.prefetch(1);
 
-        console.log('[<--__-->] Waiting for messages in the %s. queue', queue);
+        console.log('[-__-] Waiting for messages in the %s queue', queue);
 
-        const msg = await channel.consume(queue, null, {noAck: false});
+        let receipt = {};
 
-        //TODO Add logic to process the raw receipt
-        //TODO send the processed receipt to the processed queue
+        await channel.consume(queue, (data) => {
+            const msg = JSON.parse(data.content)[0];
+
+            console.log('[o_o] Consuming message as: ', data.fields.consumerTag);
+
+            const products = mineProducts(msg);
+            const paymentInfo = minePaymentInfo(msg);
+            const storeInfo = mineStoreInfo(msg);
+            const receiptNumber = msg.subArray[0]['Receipt_Number'];
+            receipt = {
+                products,
+                paymentInfo,
+                storeInfo,
+                receiptNumber,
+            };
+
+            channel.ack(data);
+        }, {noAck: false});
+
+        const processedQueue = 'processed-data';
+        await channel.assertQueue(processedQueue, { durable: true });
+        channel.sendToQueue(processedQueue, Buffer.from(JSON.stringify(receipt)), { persistent: true });
+
+        console.log('[o] The following message was successfully processed & sent to the %s queue:', processedQueue, receipt);
 
         await channel.close();
         await conn.close();
-
+        
         return {
             statusCode: 200,
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({'msg': 'Successful'}),
-        };
+        };  
     } catch (error) {
         console.log(error);
 
@@ -40,6 +62,47 @@ exports.handler = async () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({'error': 'An Error occurred'}),
-        }
+        };
     }
 };
+
+function mineProducts(data) {
+    const products = [];
+
+    data.subArray.forEach((receiptItem) => {
+        const product = {
+            productName : receiptItem['Prod_Name'],
+            productDescription : receiptItem['Prod_Desc'],
+            productPrice : receiptItem['Price'],
+            productSupplierName : receiptItem['Supplier_Name'],
+            productSupplierContact : receiptItem['Supplier_Contact'],
+            productSupplierLocation : receiptItem['Supplier_Location'],
+        };
+
+        products.push(product);
+    });
+
+    return products;
+}
+
+function mineStoreInfo(data) {
+    const receiptItem = data.subArray[0];
+    
+    return {
+        storeName : receiptItem['Store_Name'],
+        storeContact: receiptItem['Store_Contact'],
+        storeStreetName: receiptItem['Street_Name'],
+        storeCity: receiptItem['City'],
+        storeZipCode: receiptItem['Zip_Code'],
+        storeProvince: receiptItem['Province'],
+    };
+}
+
+function minePaymentInfo(data) {
+    const receiptItem = data.subArray[0];
+    
+    return {
+        customerPaymentOption : receiptItem['Cust_Pay_Opt'],
+        customerMuskedCardNumber : receiptItem['CardNumber'],
+    };
+}
